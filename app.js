397d01a857d53209acea8c6d81b3a5a433a19d6f
Kevin Tokheim <katokh89@gmail.com>
2015-10-02 13:28:36 -0500

    add partials/header.html and header view to app.js.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var nicksFlicks = angular.module("nicksFlicks", ['ui.router']);
+
+nicksFlicks.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
+        url: "",
+        views: {
+            'header': {
+                templateUrl: "partials/header.html",
+            },
+        }
+    });
+});
