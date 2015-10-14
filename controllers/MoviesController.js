c180fa45510992a6c8d1e97ac525cc439048968d
JohnKevin <student@epicodus.com>
2015-10-14 09:29:59 -0700

    fix review display

--- controllers/MoviesController.js
+++
@@ -1,5 +0,0 @@
-nicksFlicks.controller("MoviesCtrl", function MoviesCtrl($scope, MoviesFactory){
-    $scope.movies = MoviesFactory.movies;
-    $scope.MoviesFactory = MoviesFactory;
-    // $scope.movie = UtilitiesFactory.findById(MoviesFactory.movies, $stateParams.movieId);
-});
--- services/UtilitiesFactory.js
+++ services/UtilitiesFactory.js
@@ -1,12 +1,13 @@
-nicksFlicks.factory('UtilitiesFactory', function(){
-    return {
-        findById: function(collection, id){
+nicksFlicks.factory('UtilitiesFactory', function UtilitiesFactory(){
+  var factory = {};
+
+        factory.findById = function(collection, id){
             for (var i = 0; i < collection.length; i++){
                 if(collection[i].id == id){
                     return collection[i];
                 }
-                return null;
             }
         }
-    };
+
+    return factory;
 });
